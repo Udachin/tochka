7a62d922c0427c7a4bbf11a817ea9cd651aab452
Kirill Udachin <k.udachin@ibc.ru>
2020-08-02 23:54:49 +0500

    очистка кеша забронированного товара

--- app/Http/Controllers/OrderCreateController.php
+++ app/Http/Controllers/OrderCreateController.php
@@ -8,6 +8,7 @@ use App\OrderBookerService;
 use App\OrderInfoMapper;
 use App\ProductRequest;
 use App\ProductRequests;
+use Illuminate\Redis\RedisManager;
 
 class OrderCreateController extends Controller
 {
@@ -25,9 +26,10 @@ class OrderCreateController extends Controller
      * Store a newly created resource in storage.
      *
      * @param \Illuminate\Http\Request $request
+     * @param RedisManager $redis
      * @return \Illuminate\Http\Response
      */
-    public function store(CreateOrderRequest $request)
+    public function store(CreateOrderRequest $request, RedisManager $redis)
     {
         LogFacade::info('Создание заказа');
         $request->validated();
@@ -43,6 +45,11 @@ class OrderCreateController extends Controller
         }
 
         $orderId = $this->orderBookerService->book($orderInfo, $productRequests);
+
+        // Очистка кеша забронированных товаров
+        foreach ($request->orders as $requestOrder) {
+            $redis->connection()->del('product.' . $requestOrder["id"]);
+        }
 
         return response()->json([
             'orderId' => $orderId,
